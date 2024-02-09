69ecb32b10394ba9b68273d781badd64f1c9c35b
AliSeifUbuntu <aliseifubuntu@gmail.com>
2024-02-09 20:11:36 +0330

    calculate quetion of 10 chaos article

--- mean-field api2 two layer.py
+++ mean-field api2 two layer.py
@@ -1,4 +1,5 @@
 
+import numpy as np
 def p_i(k,r,l,wa,wb,Gamma):
     return complex((-pow(k,2)*pow(r,2)-2*pow(l,2)+2*l*(wa+wb-2*Gamma))/(2*pow(l,2)))
 def q_i(k,r,l,wa,wb,Gamma):
@@ -20,14 +21,38 @@ def x3_i(k,r,l,wa,wb,Gamma):
     return complex(-(U_i(k,r,l,wa,wb,Gamma)+V_i(k,r,l,wa,wb,Gamma))/2-((pow(3,1/2)*(U_i(k,r,l,wa,wb,Gamma)-V_i(k,r,l,wa,wb,Gamma)))/2)*1j-p_i(k,r,l,wa,wb,Gamma)/3)
 
 def main():
-    k=2
-    r=1
-    l=1
-    wa=-0.5
-    wb=0.5
-    Gamma=1
-    print(x3_i(k,r,l,wa,wb,Gamma))
+    k=2.6
+    l=10
+    N=1000
+    
+    file = open("omega.txt", "w")
+    file.write('r\tGamma\n')
+    for rr in range (0,100):
+        r=rr/100
+        for g in range (0,1000):
+            Gamma=g/100#9.4375
 
+            sum_gamma=0
+            for i in range (1,N+1):
+                wa=-0.5+(i-1)/(N-1)
+                wb=-wa
+                #print(f'wa={wa}\tx3_({i+5})={x3_i(k,r,l,wa,wb,Gamma)}')
+                sum_gamma+=(wa+l*x3_i(k,r,l,wa,wb,Gamma))
+            sum_gamma=sum_gamma/N
+            check=0
+            if (Gamma==round(np.real(sum_gamma),2)):
+                check=1
+                file.write(f'{r}\t{Gamma}\n')
+                print(f'gamma_total=\t{round(np.real(sum_gamma),2)}\t{r}\t{check}')
+
+    '''r_last=0
+    for i in range (1,N+1):
+        wa=-0.5+(i-1)/(N-1)
+        tetha=-np.arcsin((sum_gamma-wa-l*x3_i(k,r,l,wa,wb,Gamma))/k*r)
+        #print(f'wa={wa}\ttetha-say={tetha}')
+        r_last+=np.cos(tetha)
+    r_last=r_last/N
+    print(f'\r_last={r_last}')'''
     pass
 
 if __name__=="__main__":
